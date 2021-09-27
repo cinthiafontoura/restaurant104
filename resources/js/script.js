e1a7c4c829ec179eba0522c0913963f35b1e5984
Cinthia Fontoura <cinthiafontouras@gmail.com>
2021-09-27 17:46:47 +0100

    final - version one

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,30 +1,30 @@
-$(document).ready(function() {
-  // Sticky nav
-  var waypoint = new Waypoint({
-    element: $('.js--section-content'),
-    handler: function(direction) {
-      if (direction == "down") {
-        $('nav').addClass('sticky');
-      } else {
-        $('nav').removeClass('sticky');
-      }
-    },
-    offset: '300px;'
-  });
+$(document).ready(function () {
+	// Sticky nav
+	var waypoint = new Waypoint({
+		element: $('.js--section-content'),
+		handler: function (direction) {
+			if (direction == 'down') {
+				$('nav').addClass('sticky');
+			} else {
+				$('nav').removeClass('sticky');
+			}
+		},
+		offset: '400px;',
+	});
 
-  // Mobile nav
-  $(".js--nav-icon").click(() => {
-    const nav = $(".js--main-nav");
-    nav.slideToggle(200);
+	// Mobile nav
+	$('.js--nav-icon').click(() => {
+		const nav = $('.js--main-nav');
+		nav.slideToggle(200);
 
-    const icon = $(".js--nav-icon ion-icon");
+		const icon = $('.js--nav-icon ion-icon');
 
-    console.log(icon[0].name);
+		console.log(icon[0].name);
 
-    if (icon[0].name === "menu") {
-      icon[0].name = "close";
-    } else {
-      icon[0].name = "menu";
-    }
-  });
+		if (icon[0].name === 'menu') {
+			icon[0].name = 'close';
+		} else {
+			icon[0].name = 'menu';
+		}
+	});
 });
