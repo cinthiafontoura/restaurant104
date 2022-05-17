2f933dfffe0b2a5c83864a644d4f591d24d6fd5e
Cinthia Fontoura <cinthiafontouras@gmail.com>
2022-05-17 23:35:00 +0100

    Add burger menu for small devices

--- dist/javascript/index.js
+++ dist/javascript/index.js
@@ -1,7 +1,16 @@
-const botaoMenu = document.querySelector('.menu-mobile')
-const menuLateral = document.querySelector('.mobile-navbar')
+const open_btn = document.querySelector('.open-btn')
+const close_btn = document.querySelector('.close-btn')
+const mobile_links = document.querySelectorAll('.mobile-link')
+const nav = document.querySelectorAll('.nav')
 
-botaoMenu.addEventListener('click', () => {
-    menuLateral.classList.toggle('mobile-navbar--ative')
+open_btn.addEventListener('click', () => {
+  nav.forEach(nav_el => nav_el.classList.add('visible'))
 })
 
+close_btn.addEventListener('click', insisible)
+
+mobile_links.forEach(link_el => link_el.addEventListener('click', insisible))
+
+function insisible() {
+  nav.forEach(nav_el => nav_el.classList.remove('visible'))
+}
