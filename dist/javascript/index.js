410faa5ec85169b3c0611b9ef41de63387fdc952
Cinthia Fontoura <cinthiafontouras@gmail.com>
2022-10-06 14:24:50 +0100

    Update menus and book a table btn

--- dist/javascript/index.js
+++ dist/javascript/index.js
@@ -17,27 +17,37 @@ function insisible() {
 
 
 // Get the modal
-var modal = document.getElementById("myModal");
+let modal = document.getElementById("myModal");
+let modal2 = document.getElementById("myModal2");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+let btn = document.getElementById('myBtn');
+let btn2 = document.getElementById('myBtn2');
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+let span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
 }
 
+btn2.onclick = function() {
+  modal.style.display = "block";
+}
+
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
+  modal2.style.display = "none";
 }
+
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+  } else if (event.target == modal2) {
+    modal2.style.display = "none";
   }
 }
